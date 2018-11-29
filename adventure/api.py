ac66de42162dc37a7f912c7f413742b49df2301a
Lisa Canini <three_nineteen@yahoo.com>
2018-11-28 20:30:55 -0800

    restarted project, working through say

--- adventure/api.py
+++ adventure/api.py
@@ -63,5 +63,17 @@ def move(request):
 @csrf_exempt
 @api_view(["POST"])
 def say(request):
-    # IMPLEMENT
-    return JsonResponse({'error':"Not yet implemented"}, safe=True, status=500)
+    #get user and roominfo
+    player = request.user.player
+    player_id = player.id
+    room = player.room()
+    #get inputted message
+    data = json.loads(request.body)
+    message = data['message']
+    #get players in room
+    players = room.playerNames(player_id)
+    currentPlayerUUIDs = room.playerUUIDs(player_id)
+    for p_uuid in currentPlayerUUIDs:
+        pusher.trigger(f'p-channel-{p_uuid}', u'broadcast', {'message':f'{player.user.username} says {message}'})
+
+    return JsonResponse({'name':player.user.username, 'title':room.title, 'description':room.description, 'players':players, 'error_msg':""}, safe=True, status=200)
